510c38b19f7c386b51685c199c095486c193312b
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-24 10:37:24 -0800

    test 3 good pass on player win conditions
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,25 @@
 class RPS
 
-  def wins?(player1, player2)
+  def initialize()
+    @rps_plays = { 1 => "rock", 2 => "paper", 3 => "scissors" }
+    @computer = rand(1..3)
+    @player_win_count = 0
+  end
 
+  def rps_plays
+    @rps_plays
+  end
+
+  def wins?(user_input, computer)
+    player_1 = @rps_plays.fetch(user_input)
+    player_2 = @rps_plays.fetch(computer)
+    if (player_1 == "rock" && player_2 == "scissors") || (player_1 == "paper" && player_2 == "rock") || (player_1 == "scissors" && player_2 == "paper")
+      true
+    elsif ()
+    end
   end
 
 end
+
+# rand(1..3)
+
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,28 @@
 require ('rspec')
 require ('main')
 
-describe('RPS#wins') do
-  it("returns true if rock is the object and scissors is the argument") do
-    game = RPS.new()
-    expect(game.wins?("rock", "scissors")).to(eq(true))
+describe(RPS) do
+  describe('#rps_plays') do
+    it('reads the value of a new RPS instance') do
+      game = RPS.new()
+      expect(game.rps_plays()).to(eq({1=>"rock", 2=>"paper", 3=>"scissors"}))
+    end
   end
+  describe('wins?') do
+    it("returns true if player 1 chooses rock(1) and computer chooses scissors(3) ") do
+      game = RPS.new()
+      expect(game.wins?(1, 3)).to(eq(true))
+    end
+    it("returns true if player 1 wins") do
+      game = RPS.new()
+      expect(game.wins?(1, 3)).to(eq(true))
+      expect(game.wins?(2, 1)).to(eq(true))
+      expect(game.wins?(3, 2)).to(eq(true))
+    end
+    
+  end
+
+
 end
+
+
